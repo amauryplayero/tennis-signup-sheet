96c28543bf09711bf3202f6b7389f1129389533f
Amaury <amaury.lopezdiaz@gmail.com>
2023-09-14 17:23:26 -0400

    checking for changes

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 
 const perfomance = performance.getEntries()
 console.log(perfomance[0].type)
+
+if(!perfomance[0].type){
+    document.querySelector('.div').innerHTML = "IT FROM NFC!"
+}else {
+    document.querySelector('.div').innerHTML = "regular ass navigate or som"
+}
