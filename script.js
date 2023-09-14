328be38a53cc2c3b9b5b172f35e28c2e4ede38db
Amaury <amaury.lopezdiaz@gmail.com>
2023-09-14 17:31:57 -0400

    hellow

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 
 const perfomance = performance.getEntries()
-console.log(perfomance[0].type)
+console.log(perfomance)
 
 if(!perfomance[0].type){
     document.querySelector('.div').innerHTML = "IT FROM NFC!"
+    console.log(perfomance)
 }else {
     document.querySelector('.div').innerHTML = `${perfomance[0].type}`
+    console.log(perfomance)
 }
