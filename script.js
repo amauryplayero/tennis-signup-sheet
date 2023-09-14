cd2ea60c6f44affaec21b2209e4b8f40cbd9ff6c
Amaury <amaury.lopezdiaz@gmail.com>
2023-09-14 17:39:54 -0400

    yes yes

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ if(!perfomance[0].type){
     document.querySelector('.div').innerHTML = `${perfomance[0].type}`
     console.log(perfomance)
 }
+
+const showLogButton = document.getElementById('show-log')
+
+showLogButton.addEventListener('click', (e)=>{return console.log(perfomance)})
